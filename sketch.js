74b51597d53c4adcb6e44b52f2af1bb39aa947ef
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-07-30 18:03:13 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,10 +18,8 @@ function setup() {
   canvas = createCanvas(1200,600);
   engine = Engine.create();
   world = engine.world;
-  angle = -PI / 4;
-  ground = new Ground(0, height - 1, width * 2, 1);
+
   tower = new Tower(150, 350, 160, 310);
-  cannon = new Cannon(180, 110, 100, 50, angle);
   
 
 }
@@ -33,11 +31,9 @@ function draw() {
   
 
   Engine.update(engine);
-  ground.display();
   
 
   cannon.display();
-  tower.display();
   
  
 }
